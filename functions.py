11c867de88e67845f1be287c4e4051c93b5bbebc
Gaurav Raj <techw803@gmail.com>
2021-10-05 05:04:29 +0530

    Added Various Functionality...

--- functions.py
+++ functions.py
@@ -22,7 +22,10 @@ def basic_scan(domain):
     print("[[bold yellow]+[/bold yellow]] [bold cyan]IP Address[/bold cyan]: [bold green]{}[/bold green]".format(socket.gethostbyname(domain)))
     print("[[bold yellow]+[/bold yellow]] [bold cyan]Server[/bold cyan]: [bold green]{}[/bold green]".format(req.headers['Server']))
     print("[[bold yellow]+[/bold yellow]] [bold cyan]CMS[/bold cyan]: [bold red]Could Not Detect[/bold red]")
-    print("[[bold yellow]+[/bold yellow]] [bold cyan]Cloudflare[/bold cyan]: [bold red]Not Detect[/bold red]")
+    if (req.headers['Server'] == 'cloudflare'):
+        print("[[bold yellow]+[/bold yellow]] [bold cyan]Cloudflare[/bold cyan]: [bold green]Detected[/bold green]")
+    else:
+        print("[[bold yellow]+[/bold yellow]] [bold cyan]Cloudflare[/bold cyan]: [bold red]Not Detect[/bold red]")
     rreq = requests.get(url+'/robots.txt')
     if(rreq.status_code != 404):
         print("[[bold yellow]+[/bold yellow]] [bold cyan]Robots.txt[/bold cyan]: [bold green]Found[/bold green]")
@@ -44,3 +47,56 @@ def whois(domain):
     print(data.get_text())
     print(
         "---------------------------------[ [bold green]End of the Information[/bold green] ]------------------------------------")
+
+
+def dnslookup(domain):
+    url = 'https://api.hackertarget.com/dnslookup/?q={}'.format(domain)
+    req = requests.get(url)
+    print(
+        "------------------------------------[ [bold green]DNS Lookup[/bold green] ]------------------------------------")
+    print(req.text)
+    print(
+        "--------------------------------[ [bold green]DNS Lookup Ends Here[/bold green] ]------------------------------------")
+
+
+def geoiplookup(domain):
+    ip = socket.gethostbyname(domain)
+    url = 'https://api.hackertarget.com/geoip/?q={}'.format(ip)
+    req = requests.get(url)
+    print(
+        "------------------------------------[ [bold green]GEO-IP Lookup[/bold green] ]------------------------------------")
+    print(req.text)
+    print(
+        "--------------------------------[ [bold green]GEO-IP Lookup Ends Here[/bold green] ]------------------------------------")
+
+
+def subnetcalc(domain):
+    ip = socket.gethostbyname(domain)
+    url = 'https://api.hackertarget.com/subnetcalc/?q={}'.format(ip)
+    req = requests.get(url)
+    print(
+        "------------------------------------[ [bold green]Subnet Calculator[/bold green] ]------------------------------------")
+    print(req.text)
+    print(
+        "------------------------------------[ [bold green]Content Ends Here[/bold green] ]------------------------------------")
+
+
+def subdomains(domain):
+    url = 'https://api.hackertarget.com/hostsearch/?q={}'.format(domain)
+    req = requests.get(url)
+    print(
+        "------------------------------------[ [bold green]Subdomain Finder[/bold green] ]------------------------------------")
+    print(req.text)
+    print(
+        "------------------------------------[ [bold green]Subdomain Finder[/bold green] ]------------------------------------")
+
+
+def nmapscan(domain):
+    url = 'https://api.hackertarget.com/nmap/?q={}'.format(
+        socket.gethostbyname(domain))
+    req = requests.get(url)
+    print(
+        "------------------------------------[ [bold green]NMAP Scan[/bold green] ]------------------------------------")
+    print(req.text)
+    print(
+        "--------------------------------[ [bold green]NMAP Scan Ends Here[/bold green] ]------------------------------------")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from rich import print
 import argparse
-from functions import basic_scan, whois
+from functions import basic_scan, whois, dnslookup, geoiplookup, subnetcalc, subdomains, nmapscan
 from sys import argv
 
 
@@ -53,9 +53,17 @@ def main():
         "-d", "--domain", help="Specify Target Domain", required=True)
     parser.add_argument(
         "-w", "--whois", help="Extract Whois Information for The Target Domain.", action="store_true")
+    parser.add_argument(
+        "-l", "--lookup", help="DNS Lookup for the Target Domain", action="store_true")
+    parser.add_argument(
+        "-g", "--geoip", help="GEO-IP Lookup on the Target's IP", action="store_true")
+    parser.add_argument(
+        "-n", "--subnet", help="Subnet Calculator for Target Domain's IP Address", action="store_true")
+    parser.add_argument(
+        "-s", "--subdomains", help="Find Available Subdomains for the Target's Domain", action="store_true")
+    parser.add_argument(
+        "-p", '--ports', help="Perform Nmap Scan on Target Domain's IP Address", action="store_true")
     args = parser.parse_args()
-    if (len(argv) == 3):
-        banner
     banner()
     print(
         "                            [[bold green]+[/bold green]] Target Domain: [bold green]{}[/bold green]\n".format(args.domain))
@@ -63,6 +71,21 @@ def main():
     if(args.whois):
         scan_type('Whois Recon')
         whois(args.domain)
+    elif(args.lookup):
+        scan_type('DNS Lookup')
+        dnslookup(args.domain)
+    elif(args.geoip):
+        scan_type('GEO-IP Lookup')
+        geoiplookup(args.domain)
+    elif(args.subnet):
+        scan_type('Subnet Calculator')
+        subnetcalc(args.domain)
+    elif(args.subdomains):
+        scan_type('Subdomains Finder')
+        subdomains(args.domain)
+    elif(args.ports):
+        scan_type('NMAP Scan')
+        nmapscan(args.domain)
     else:
         scan_type('Basic Scan')
         basic_scan(args.domain)
