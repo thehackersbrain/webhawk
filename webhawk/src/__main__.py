58ef544835dee25d4c41c8432c3623a88392c870
Gaurav Raj <techw803@gmail.com>
2021-10-17 23:11:17 +0530

    Updated Version Detail...

--- webhawk/src/__main__.py
+++ webhawk/src/__main__.py
@@ -25,7 +25,7 @@ def scan_type(stype):
 
 def main():
     if (len(argv) == 2 and argv[1] == '-v'):
-        print("[bold green]Webhawk 0.1.0[/bold green]")
+        print("[bold green]Webhawk 0.1.2[/bold green]")
         exit(0)
 
     # Parsing Arguments
