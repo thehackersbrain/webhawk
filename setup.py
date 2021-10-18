8015a3f041561ce2905eac639f0bdc001611fc5a
Gaurav Raj <techw803@gmail.com>
2021-10-18 18:13:10 +0530

    Updated Version Detail...

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     packages=find_packages(),
     package_data={"": ["data/*"]},
     install_requires=['requests', 'rich', 'bs4'],
-    entry_points={'console_scripts': ['webhawk=webhawk.__main__:main']},
+    entry_points={'console_scripts': ['webhawk=webhawk.src.__main__:main']},
     zip_safe=False,
 )
